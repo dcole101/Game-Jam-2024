e24562d4e48f16be31e4a6664cf9c78311b799d7
NicolasTetreaultIMD <NICOLASTETREAULT@cmail.carleton.ca>
2024-01-27 09:45:19 -0500

    Finished preciseClick controls

--- Assets/Scripts/Controls/BaseController.cs
+++ Assets/Scripts/Controls/BaseController.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public abstract class BaseController
 {
-    public abstract void UpdateControls();
+    public abstract void SetupControls(Canvas gameArea);
+    public abstract List<RaycastResult> UpdateControls(float deltaTime);
 }
